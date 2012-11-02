7e0113344392b1523881df52089b1e3d5ac9bd08
Scott Lesser <scottwlesser@gmail.com>
2012-11-02 16:19:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RdioPlaylists::Application.config.secret_token = 'de378b882c371557dd7ab5290436f3cc2c6faa47401289de7a0f4405a81e648eb6053ac634d75261af70ff446b6ab6ff7bcf30541aa4f9543f5593e91aede45b'
